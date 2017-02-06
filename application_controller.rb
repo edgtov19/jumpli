f034fce39194475d5dd65351775dcd021d3272ad
edgartovar2017 <edgartovar2017@gmail.com>
2017-02-06 12:09:29 -0500

    first commmit

---
+++ application_controller.rb
@@ -0,0 +1,21 @@
+require 'bundler'
+Bundler.require
+require_relative 'models/model.rb'
+class MyApp < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+ 
+  post '/cost' do 
+    #get important information out of params 
+    user_size=params[:size]
+    user_cost=params[:cost]
+    @result=castle(user_size, user_cost)
+    @cost=@result[0]
+    @image=@result[1]
+    erb :results
+  end
+  
+  
+end
---
+++ models/model.rb
@@ -0,0 +1,12 @@
+#Write your ruby methods here!
+
+def castle(size, cost) 
+  if size=="medium" 
+    answer=["90","http://njstatic.com/images/P/castle-2-l1.jpg"]
+    
+  elsif size=="large" 
+    answer=["120","http://njstatic.com/images/P/extra-large-fun-house1.jpg"]
+  end 
+  return answer
+  
+end
